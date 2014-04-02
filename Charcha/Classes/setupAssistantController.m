18d2529c1e8bdd3caf79c332e6236741d5a03cb1
Parthasarathi Susarla <ajaysusarla@gmail.com>
2014-04-02 23:28:54 +1100

    Add the setup assistant class.

---
+++ Charcha/Classes/setupAssistantController.m
@@ -0,0 +1,33 @@
+//
+//  setupAssistantController.m
+//  Charcha
+//
+//  Created by Parthasarathi Susarla on 02/04/14.
+//  Copyright (c) 2014 Parthasarathi Susarla. All rights reserved.
+//
+
+#import "setupAssistantController.h"
+
+@interface setupAssistantController ()
+
+@end
+
+@implementation setupAssistantController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
