5a34df8298b469bae47c8c837511306a664846e3
Partha Susarla <ajaysusarla@gmail.com>
2016-04-29 15:28:17 +1000

    Move to using QT

---
+++ charcha-main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+#include <QApplication>
+
+int main(int argc, char **argv)
+{
+        QApplication *application = new QApplication(argc, argv);
+        (void)application;
+
+
+        return 0;
+}
+
